f1f9a8996309a41202cdb1b3dbaa0ba84878c861
Mike MacDonald <crazymykl@gmail.com>
2018-10-04 22:48:40 -0400

    Redo interface to use stdin instead of args

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,26 @@ mod possibilites;
 mod programmed_iterator;
 mod square;
 
-use self::board::Board;
+use crate::board::Board;
+use std::io::{self, BufRead};
 use std::str::FromStr;
 
 fn main() {
-    for arg in std::env::args().skip(1) {
-        match Board::from_str(&arg) {
-            Ok(x) => println!(
-                "{}\n{}",
-                x,
-                x.solve()
-                    .map(|b| b.to_string())
-                    .unwrap_or_else(|| "No solution".into())
-            ),
-            e => println!("{:?}", &e),
-        }
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        solve_str(&line.expect("Can't read stdin!"))
+    }
+}
+
+fn solve_str(arg: &str) {
+    match Board::from_str(&arg) {
+        Ok(x) => println!(
+            "{}\n{}",
+            x,
+            x.solve()
+                .map(|b| b.to_string())
+                .unwrap_or_else(|| "No solution".into())
+        ),
+        e => println!("{:?}", &e),
     }
 }
